e4ed41239b3719be90bcd93e2d865a37974627ae
Pracha Phimthawong <pracha.pi@rmuti.ac.th>
2023-08-05 19:48:19 +0700

    first commit

---
+++ src/main/java/com/example/backrequestKKC/BackRequestKkcApplication.java
@@ -0,0 +1,13 @@
+package com.example.backrequestKKC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRequestKkcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackRequestKkcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backrequestKKC/BackRequestKkcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backrequestKKC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRequestKkcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
